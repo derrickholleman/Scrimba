3ba15f312cb85ccb631aa51337151337c6243758
Derrick Holleman <derrickholleman@gmail.com>
2021-08-05 18:18:03 -0500

    progress

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -1,5 +1,27 @@
-// document.getElementById('count-el').innerHTML = 5
-
+// set totalCount to HTML 
+totalCount = document.getElementById('count-el')
+// store save paragraph in a variable
+let saveCount = document.getElementById('save') 
+// initialize count variable as 0
 let count = 0
-console.log(count)
+
+function increment() {
+    count += 1
+    return totalCount.innerHTML = count // html now equals count
+}
+
+function save() {
+    // store current count in variable that's called each time the save button is pressed
+    let countStr = `${count} - `
+    return saveCount.innerHTML += countStr
+}
+
+// welcome 
+welcomeParagraph = document.getElementById('welcome')
+let userName = 'Derrick'
+let greeting = 'Hello,'
+welcomeParagraph.innerHTML = `${greeting} ${userName}`
+welcomeParagraph.innerHTML += ' 😄'
+
+
 
