dfb9ba2bd3505dfb95eefded61b7c4c3aa44232f
Derrick Holleman <derrickholleman@gmail.com>
2021-08-06 22:10:16 -0500

    reset btn and disable btn

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -12,6 +12,8 @@ let sum = cardHolder.reduce((a, b) => a + b)
 let message = document.getElementById('message-el')
 let displaySum = document.getElementById('sum-el')
 let displayCards = document.getElementById('cards-el')
+// set state for new card button
+document.getElementById('newCard-btn').disabled = true
 
 function startGame() {
     renderGame()
@@ -22,10 +24,13 @@ function renderGame() {
     // display player status message
     if (sum <= 20) {
         message.textContent = 'Do you want to draw a new card?'
+        document.getElementById('newCard-btn').disabled = false
     } else if (sum === 21) {
         message.textContent = 'You win!'
+        document.getElementById('newCard-btn').disabled = true
     } else {
         message.textContent = "You're out of the game!"
+        document.getElementById('newCard-btn').disabled = true
     }
 
     // display cards array as text 
@@ -45,4 +50,17 @@ function newCard() {
     // add new card to card array
     cardHolder.push(card)
     renderGame()
+}
+
+function resetGame() {
+    // reset text
+    displaySum.textContent = `Sum: `
+    displayCards.textContent = `Cards: `
+    message.textContent = 'Want to play a round?'
+
+    // reset cards 
+    cardHolder = [Math.floor(Math.random() * (11 - 2 + 1) + 2), Math.floor(Math.random() * (11 - 2 + 1) + 2)]
+
+    // reset sum
+    sum = cardHolder.reduce((a, b) => a + b)
 }
