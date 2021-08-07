f7d878bb60fe4325cc0bafc7b2099b5a12807c33
Derrick Holleman <derrickholleman@gmail.com>
2021-08-06 21:14:42 -0500

    done for the day

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,27 +1,33 @@
-// function getTwoCards(min, max) {
-//     let firstCard = Math.floor(Math.random() * (max - min) + min)
-//     let secondCard = Math.floor(Math.random() * (max - min) + min)
-//     let sum = firstCard + secondCard
-//     return sum
-// }
-// console.log(getTwoCards(2, 11))
-
 let firstCard = 14
 let secondCard = 3
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-let message =''
 
-if (sum <= 20) {
-    message = 'Do you want to draw a new card?'
-} else if (sum === 21) {
-    message = 'You win!'
-    hasBlackJack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
+let message = document.getElementById('message-el')
+let displaySum = document.getElementById('sum-el')
+let displayCards = document.getElementById('cards-el')
+
+function startGame() {
+    renderGame()
 }
-console.log(hasBlackJack)
-console.log(isAlive)
-console.log(message)
+
+function renderGame() {
+    if (sum <= 20) {
+        message.textContent = 'Do you want to draw a new card?'
+    } else if (sum === 21) {
+        message.textContent = 'You win!'
+        hasBlackJack = true
+    } else {
+        message.textContent = "You're out of the game!"
+        isAlive = false
+    }
+    displayCards.textContent = `Cards: ${firstCard} ${secondCard}`
+    displaySum.textContent = `Sum: ${sum}`
+}
+
+function newCard() {
+    let card = Math.floor(Math.random() * (11 - 2 + 1) + 2)
+    sum += card
+    renderGame()
+}
