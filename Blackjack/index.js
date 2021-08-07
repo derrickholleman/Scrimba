de3ea80e4584d63ed45d5d89294682cbdc5ccd44
Derrick Holleman <derrickholleman@gmail.com>
2021-08-06 21:51:17 -0500

    created card array

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,6 +1,13 @@
+// create cards
 let firstCard = Math.floor(Math.random() * (11 - 2 + 1) + 2)
 let secondCard = Math.floor(Math.random() * (11 - 2 + 1) + 2)
-let sum = firstCard + secondCard
+
+// create card array 
+let cardHolder = []
+cardHolder.push(firstCard, secondCard)
+
+// get sum of current cards
+let sum = cardHolder.reduce((a, b) => a + b)
 
 let message = document.getElementById('message-el')
 let displaySum = document.getElementById('sum-el')
@@ -11,6 +18,8 @@ function startGame() {
 }
 
 function renderGame() {
+
+    // display player status message
     if (sum <= 20) {
         message.textContent = 'Do you want to draw a new card?'
     } else if (sum === 21) {
@@ -18,12 +27,22 @@ function renderGame() {
     } else {
         message.textContent = "You're out of the game!"
     }
-    displayCards.textContent = `Cards: ${firstCard} ${secondCard}`
+
+    // display cards array as text 
+    displayCards.textContent = `Cards: ${cardHolder.map((card) => ` ${card}`)}`
+
+    // display sum
     displaySum.textContent = `Sum: ${sum}`
 }
 
 function newCard() {
+
+    // draw new card with random number
     let card = Math.floor(Math.random() * (11 - 2 + 1) + 2)
+    // add card value to current sum
     sum += card
+
+    // add new card to card array
+    cardHolder.push(card)
     renderGame()
 }
