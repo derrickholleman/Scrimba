ad6ce740a298936716e5bf5043b3abdb7f232fc2
Derrick Holleman <derrickholleman@gmail.com>
2021-08-16 18:24:19 -0500

    local storage

--- Chrome-Extension/index.js
+++ Chrome-Extension/index.js
@@ -3,20 +3,34 @@ const inputBtn = document.getElementById('input-btn')
 const inputEl = document.getElementById('input-el')
 const renderInput = document.getElementById('leads-el')
 
+// fetch leads from storage by it's key name
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
+
 
 inputBtn.addEventListener('click', () => {
-    renderLeads()
+    // don't allow push if value is empty, else push value
+    inputEl.value.length === 0 ? null : myLeads.push(inputEl.value)
     // reset input field 
     inputEl.value = ''
+    // save leads leads array to localStorage as a string
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+
+    renderLeads()
 })
 
 function renderLeads() {
     // declare string to store leads
     let listItems = ''
-    // don't allow push if value is empty, else push value
-    inputEl.value.length === 0 ? null : myLeads.push(inputEl.value)
+
     // map leads to <ul> tag as <li> by using innerHTML
-    listItems = myLeads.map((lead) => `<li><a href=${lead} target=_blank>${lead}</a></li>`).join('')
+    listItems = myLeads.map((lead) => 
+    `
+        <li>
+            <a href=${lead} target=_blank>${lead}</a>
+        </li>
+    `
+    ).join('')
+
     // only manipulating DOM once, instead of on each render
     renderInput.innerHTML = listItems
 }
