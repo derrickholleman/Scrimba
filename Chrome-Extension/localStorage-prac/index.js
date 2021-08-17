147c63cfaf3bf267c9927c09313314a8031182f5
Derrick Holleman <derrickholleman@gmail.com>
2021-08-16 23:30:20 -0500

    comments

--- Chrome-Extension/localStorage-prac/index.js
+++ Chrome-Extension/localStorage-prac/index.js
@@ -10,11 +10,14 @@
 // myLeads = JSON.stringify(myLeads)
 // console.log(typeof myLeads)
 
+const renderText = document.getElementById('text')
+
 let names = ['Derrick', 'Stef', 'Julie']
 names.push('Sam')
 
-const renderText = document.getElementById('text')
-
+// push array to local storage as a string
 localStorage.setItem('names', JSON.stringify(names))
+// set local storage data as a variable, while turning it back into an array with JSON.parse
 let text = JSON.parse(localStorage.getItem(('names')))
+// render local storage data as an array joined by spaces
 renderText.textContent = text.join(' ')
