cb1c8a42316a374e2a6baf18be0bd2ea90bcc1af
Derrick Holleman <derrickholleman@gmail.com>
2021-09-22 10:51:41 -0500

    form example

--- Chrome-Extension/localStorage-prac/index.js
+++ Chrome-Extension/localStorage-prac/index.js
@@ -1,7 +1,10 @@
 const renderText = document.getElementById('text')
+const inputEl = document.getElementById('amount')
 
 let names = ['Derrick', 'Stef', 'Julie']
 names.push('Sam')
+
+let amount = 0
 
 // push array to local storage as a string
 localStorage.setItem('names', JSON.stringify(names))
@@ -11,3 +14,8 @@ let text = JSON.parse(localStorage.getItem('names'))
 
 // render local storage data as an array joined by spaces
 renderText.textContent = text.join(' ')
+
+function submitForm() {
+    amount = inputEl.value
+    localStorage.setItem('amount', JSON.stringify(amount))
+}
